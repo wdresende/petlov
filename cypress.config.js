7845bbd464f8606f45f9b6c10649131055672027
Diego Resende <wdresende@gmail.com>
2026-02-22 18:17:00 -0300

    adicionando github actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   e2e: {
+    projectId: "2emyhi",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://petlov.vercel.app'
+    baseUrl: "https://petlov.vercel.app",
   },
 };
